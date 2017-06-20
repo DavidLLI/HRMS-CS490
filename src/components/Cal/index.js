416da410c73279155ab5483a4deb6c33008d2aa3
Renee Ren <reneeren@v1020-wn-24-74.campus-dynamic.uwaterloo.ca>
2017-06-20 13:58:40 -0400

    deleted some extra files on calendar

--- src/components/Cal/index.js
+++ src/components/Cal/index.js
@@ -1,11 +1,8 @@
 import styles from 'react-big-calendar/lib/css/react-big-calendar.css'
 import React, { Component } from 'react';
 import events from './events.js'
-import Basic from './Basic';
-import Timeslots from './TimeSlots';
-import Selectable from './Selectable';
 import { Redirect } from 'react-router-dom';
-import { Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
+import { Grid, Row, Col, Nav, NavItem, ButtonGroup, Button, DropdownButton, MenuItem } from 'react-bootstrap';
 
 import AuthStore from '@stores/Auth.store';
 import BigCalendar from 'react-big-calendar';
@@ -16,53 +13,36 @@ let allViews = Object.keys(BigCalendar.views).map(k => BigCalendar.views[k])
 
 BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
-/*
-const Cal = props => (
-  <div>
-    <BigCalendar
-      events={events}
-      startAccessor='startDate'
-      endAccessor='endDate'
-      style={{height: 700}}
-    />
-  </div>
-);
-*/
+
+
 
 class Cal extends Component{
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-      activeTab: 'basic',
+      selectable: true,
+      //activeTab: 'basic',
     };
   }
-
-
-  handleTabSelect = (selectedTab) => {
-    this.setState({ activeTab: selectedTab });
-  };
-
-  renderForm = () => {
-    if (this.state.activeTab === 'timeslots') return <timeslots/>;
-    return <Basic/>;
-  };
 
 
   render() {
     return (
       <div>
       <Row>
-        <NavItem eventKey={'basic'} href="basic">Basic</NavItem>
-        <NavItem eventKey={'selectable'} href="selectable">Selectable</NavItem>
-        <NavItem eventKey={'timeslots'} href="timeslots">TimeSlots</NavItem>
-      </Row>
-      <Row>
       <BigCalendar
             {...this.props}
+            selectable
             events={events}
             views={allViews}
+            scrollToTime={new Date(1970, 1, 1, 6)}
             defaultDate={new Date(2017, 3, 1)}
+            onSelectEvent={event => alert(event.title)} //add event here
+            onSelectSlot={(slotInfo) => alert(
+              `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+              `\nend: ${slotInfo.end.toLocaleString()}`
+            )}
             style={{height: 700}}
             />
             </Row>
@@ -72,62 +52,7 @@ class Cal extends Component{
     }
 }
 
-/*
-
-class Cal extends Component{
 
 
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      activeTab: 'basic',
-    };
-  }
-
-  handleTabSelect = (selectedTab) => {
-    this.setState({ activeTab: selectedTab });
-  };
-
-  renderForm = () => {
-    if (this.state.activeTab === 'selectable') return <Selectable/>;
-    return <Basic/>;
-  };
-
-  render(){
-    if (AuthStore.isLoggedIn) {
-      return <Redirect to={{ pathname: '/' }} />;
-    }
-
-    return (
-      <div className='container-fluid'>
-        <Grid>
-          <Row>
-            <Nav bsStyle="tabs" activeKey={this.state.activeTab} onSelect={this.handleTabSelect}>
-              <NavItem eventKey={'basic'} href="basic">Basic</NavItem>
-              <NavItem eventKey={'selectable'} href="selectable">Selectable</NavItem>
-            </Nav>
-          </Row>
-          <Row>
-            <Col>{ this.renderForm() }</Col>
-          </Row>
-        </Grid>
-      </div>
-  );
-
-  }
-
-}
-/*
-const Cal = props => (
-  <div>
-    <BigCalendar
-      events={events}
-      startAccessor='startDate'
-      endAccessor='endDate'
-      style={{height: 700}}
-    />
-  </div>
-);
-*/
 export default Cal;
