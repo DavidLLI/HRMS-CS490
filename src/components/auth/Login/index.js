1697d98d41a8bd2b0cc636b3013efa112ad41b39
Yang Li <yang.li@uwaterloo.ca>
2017-06-20 20:09:04 -0400

    login and signup

--- src/components/auth/Login/index.js
+++ src/components/auth/Login/index.js
@@ -3,26 +3,61 @@ import { Redirect } from 'react-router-dom';
 import { PageHeader } from 'react-bootstrap';
 import AuthStore from '@stores/Auth.store';
 import { observer } from 'mobx-react';
+import './login.css';
 
 @observer class Login extends Component {
-  static displayName = 'Login';
 
-  onLogin() {
-  	AuthStore.logIn();
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(event) {
+  	this.setState({username: event.target.value});
+  }
+
+  handlePasswordChange(event) {
+  	this.setState({password: event.target.value});
+  }
+
+  handleSubmit(event) {
+  	event.preventDefault();
+  	AuthStore.logIn(this.state.username, this.state.password);
   }
 
   render() {
-  	console.log('re render');
   	if (AuthStore.isLoggedIn) {
   		return <Redirect to={{ pathname: '/' }} />;
   	}
 
     return (
-      <PageHeader>
-        <button onClick={this.onLogin}>
-        	Log In
-        </button>
-      </PageHeader>
+      	<form className="loginForm" onSubmit={this.handleSubmit}>
+      		<div className="username">
+      			<label>
+		    		Username:
+		    		<input type="text" className="usernameField" value={this.state.username} onChange={this.handleUsernameChange} />
+		  		</label>
+      		</div>
+      		<div className="password">
+      			<label>
+		    		Password:
+		    		<input type="password" className="passwordField" value={this.state.password} onChange={this.handlePasswordChange} />
+		  		</label>
+      		</div>
+		  	<div className="loginBtn">
+		  		<input type="submit" value="Log In" className="loginButton" />
+		  	</div>
+		  	{AuthStore.logInFail && 
+				<label className='loginError'>Login failed</label>
+			}
+		</form>
     );
   }
 }
--- src/stores/Auth.store.js
+++ src/stores/Auth.store.js
@@ -1,10 +1,46 @@
 import { observable, action } from 'mobx';
+import axios from 'axios';
 
 class Auth {
   @observable isLoggedIn = false;
+  @observable logInFail = false;
 
-  @action logIn() {
-  	this.isLoggedIn = true;
+  @observable signupError = false;
+
+  @observable username = '';
+
+  @observable activeTab = 'login';
+
+  @action setTab(activeTab) {
+  	this.activeTab = activeTab;
+  }
+
+  @action signUp(username, password) {
+  	axios.post('http://localhost:4000/api/employee/signup', {
+  			username: username,
+  			password: password
+  		})
+  		.then(() => {
+  			this.setTab('login');
+  			this.signupError = false;
+  		})
+  		.catch((error) => {
+  			this.signupError = error.response.data.message;
+  		});
+  }
+
+  @action logIn(username, password) {
+  	axios.get('http://localhost:4000/api/employee/username/' + username + '/password/' + password)
+  		.then((data) => {
+  			if (data.status === 200) {
+  				this.username = username;
+  				this.isLoggedIn = true;
+  			}
+  		})
+  		.catch((error) => {
+  			this.logInFail= true;
+  			console.log(error);
+  		});
   }
 
   @action logOut() {
